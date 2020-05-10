c7d8231ea45721f8a0a08298faa000e11a989372
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2020-05-10 10:54:27 +0300

    First version
    It works, it has tests and it has benchmark

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # CSharp-ChaCha20-NetStandard-21
- .Net Standard 2.1 compatible managed ChaCha20 Cipher implementation (encryption and decryption) in C#
+Managed .Net Standard 2.1 compatible [ChaCha20](https://en.wikipedia.org/wiki/Salsa20#ChaCha_variant) cipher written in C#
+
+## Build status
+Not yet
+
+## Why?
+Because I had some extra time
+
+## Differences between this and .Net Standard 2.0 versions
+There is also an .Net Standard 2.0 compatible version available in [CSharp-ChaCha20-NetStandard](https://github.com/mcraiha/CSharp-ChaCha20-NetStandard)
+
++ Uses Span<byte> and Readonly<Span> for parameters
++ Allocates less memory (176 bytes vs. 88 bytes) because it can use stackalloc
+- Slower
+
+## Origin
+
+**Scott Bennett** wrote C# implementation called [ChaCha20-csharp](https://github.com/sbennett1990/ChaCha20-csharp), which works as base for my code. That is why the license is same for both projects
+
+## How do I use this?
+
+Either copy the [CSChaCha20.cs](src/CSChaCha20.cs) to your project or use upcoming nuget package, 
+
+Then do code like  
+```csharp
+using CSChaCha20_NS21;
+
+byte[] mySimpleTextAsBytes = Encoding.ASCII.GetBytes("Plain text I want to encrypt");
+
+// Do not use these key and nonce values in your own code!
+byte[] key = new byte[32] { 142, 26, 14, 68, 43, 188, 234, 12, 73, 246, 252, 111, 8, 227, 57, 22, 168, 140, 41, 18, 91, 76, 181, 239, 95, 182, 248, 44, 165, 98, 34, 12 };
+byte[] nonce = new byte[12] { 139, 164, 65, 213, 125, 108, 159, 118, 252, 180, 33, 88 };
+uint counter = 1;
+
+// Encrypt
+ChaCha20 forEncrypting = new ChaCha20(key, nonce, counter);
+byte[] encryptedContent = new byte[mySimpleTextAsBytes.Length];
+forEncrypting.EncryptBytes(encryptedContent, mySimpleTextAsBytes);
+
+// Decrypt
+ChaCha20 forDecrypting = new ChaCha20(key, nonce, counter);
+byte[] decryptedContent = new byte[encryptedContent.Length];
+forDecrypting.DecryptBytes(decryptedContent, encryptedContent);
+
+```
+
+## Test cases
+
+You can run test cases by moving to **tests** folder and running following command
+```bash
+dotnet test
+```
+
+## Benchmarks
+
+You can run benchmarks (which compare this implementation to the original version) by moving to **benchmarks** folder and running following command
+```bash
+dotnet run -c Release
+```
+
+there are three different input sizes (64 bytes, 1024 bytes and 1 MiB) and comparisons are done between the original version (made by Scott Bennett), Net Standard 2.0 version and this project
+
+## License
+
+All the code is licensed under [ISC License](LICENSE)
---
+++ benchmarks/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+using CSChaCha20;
+using CSChaCha20_NS21;
+using ChaCha20Cipher;
+
+namespace benchmarks
+{
+    [MemoryDiagnoser]
+    public class Original_Vs_NS20_Vs_NS21
+    {
+        private const int dataLength1 = 64;
+        private const int dataLength2 = 1024;
+        private const int dataLength3 = 1024*1024;
+
+        private readonly byte[] data1;
+        private readonly byte[] data2;
+        private readonly byte[] data3;
+
+        private readonly ChaCha20Cipher.ChaCha20Cipher original1 = null;
+        private readonly ChaCha20Cipher.ChaCha20Cipher original2 = null;
+        private readonly ChaCha20Cipher.ChaCha20Cipher original3 = null;
+
+        private readonly CSChaCha20.ChaCha20 ns_20_1 = null;
+        private readonly CSChaCha20.ChaCha20 ns_20_2 = null;
+        private readonly CSChaCha20.ChaCha20 ns_20_3 = null;
+
+        private readonly CSChaCha20_NS21.ChaCha20 ns_21_1 = null;
+        private readonly CSChaCha20_NS21.ChaCha20 ns_21_2 = null;
+        private readonly CSChaCha20_NS21.ChaCha20 ns_21_3 = null;
+
+
+
+        private static readonly byte[] key = new byte[32] { 
+                                                        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 
+														0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 
+														0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 
+														0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
+                                                        };
+        
+        private static readonly byte[] nonce = new byte[12] { 0x00, 0x09, 0x00, 0x00, 0xFF, 0x20, 0x12, 0x00, 0x00, 0x8b, 0x00, 0x02 };
+        private static readonly uint counter = 13;
+
+        private byte[] outputForOriginal1;
+        private byte[] outputForOriginal2;
+        private byte[] outputForOriginal3;
+
+        private byte[] outputForNS_20_1;
+        private byte[] outputForNS_20_2;
+        private byte[] outputForNS_20_3;
+
+        private byte[] outputForNS_21_1;
+        private byte[] outputForNS_21_2;
+        private byte[] outputForNS_21_3;
+
+        public Original_Vs_NS20_Vs_NS21()
+        {
+            // Arrays for outputs
+            this.outputForOriginal1 = new byte[dataLength1];
+            this.outputForOriginal2 = new byte[dataLength2];
+            this.outputForOriginal3 = new byte[dataLength3];
+
+            this.outputForNS_20_1 = new byte[dataLength1];
+            this.outputForNS_20_2 = new byte[dataLength2];
+            this.outputForNS_20_3 = new byte[dataLength3];
+
+            this.outputForNS_21_1 = new byte[dataLength1];
+            this.outputForNS_21_2 = new byte[dataLength2];
+            this.outputForNS_21_3 = new byte[dataLength3];
+
+            // Generate inputs
+            Random rng = new Random(Seed: 1337);
+
+            this.data1 = new byte[dataLength1];
+            rng.NextBytes(this.data1);
+
+            this.data2 = new byte[dataLength2];
+            rng.NextBytes(this.data2);
+
+            this.data3 = new byte[dataLength3];
+            rng.NextBytes(this.data3);
+
+            // Set encrypters
+            this.original1 = new ChaCha20Cipher.ChaCha20Cipher(key, nonce, counter);
+            this.original2 = new ChaCha20Cipher.ChaCha20Cipher(key, nonce, counter);
+            this.original3 = new ChaCha20Cipher.ChaCha20Cipher(key, nonce, counter);
+
+            this.ns_20_1 = new CSChaCha20.ChaCha20(key, nonce, counter);
+            this.ns_20_2 = new CSChaCha20.ChaCha20(key, nonce, counter);
+            this.ns_20_3 = new CSChaCha20.ChaCha20(key, nonce, counter);
+
+            this.ns_21_1 = new CSChaCha20_NS21.ChaCha20(key, nonce, counter);
+            this.ns_21_2 = new CSChaCha20_NS21.ChaCha20(key, nonce, counter);
+            this.ns_21_3 = new CSChaCha20_NS21.ChaCha20(key, nonce, counter);
+        }
+
+    #region 64 bytes
+        [Benchmark]
+        public void Original_64_Bytes() => this.original1.EncryptBytes(this.outputForOriginal1, this.data1, dataLength1);
+
+        [Benchmark]
+        public void NS20_64_Bytes() => this.ns_20_1.EncryptBytes(this.outputForNS_20_1, this.data1, dataLength1);
+
+        [Benchmark]
+        public void NS21_64_Bytes() => this.ns_21_1.EncryptBytes(this.outputForNS_21_1, this.data1, dataLength1);
+
+    #endregion // 64 bytes
+
+    #region 1024 bytes
+        [Benchmark]
+        public void Original_1024_Bytes() => this.original2.EncryptBytes(this.outputForOriginal2, this.data2, dataLength2);
+
+        [Benchmark]
+        public void NS20_1024_Bytes() => this.ns_20_2.EncryptBytes(this.outputForNS_20_2, this.data2, dataLength2);
+
+        [Benchmark]
+        public void NS21_1024_Bytes() => this.ns_21_2.EncryptBytes(this.outputForNS_21_2, this.data2, dataLength2);
+
+    #endregion // 1024 bytes
+
+    #region 1 MiB
+        [Benchmark]
+        public void Original_1MiB_Bytes() => this.original3.EncryptBytes(this.outputForOriginal3, this.data3, dataLength3);
+
+        [Benchmark]
+        public void NS20_1Mib_Bytes() => this.ns_20_3.EncryptBytes(this.outputForNS_20_3, this.data3, dataLength3);
+
+        [Benchmark]
+        public void NS21_1Mib_Bytes() => this.ns_21_3.EncryptBytes(this.outputForNS_21_3, this.data3, dataLength3);
+
+    #endregion // 1 MiB
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Original_Vs_NS20_Vs_NS21>();
+        }
+    }
+}
